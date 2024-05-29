5335654fe2b0b56e975c30a1e9b08c5c0081cd5b
aminoxlhd <aminoxlhd@gmail.com>
2024-05-29 20:21:41 +0100

    edit Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """filter_datum function"""
     pattern = r"(?:" + separator.join(fields) + r")=([^;]+);"
     return re.sub(pattern, r"\1=" + redaction + ";", message)
