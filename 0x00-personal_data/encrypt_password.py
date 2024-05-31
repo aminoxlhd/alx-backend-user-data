34d67cc633ca7a4d949eb61d399e611d354f1387
aminoxlhd <aminoxlhd@gmail.com>
2024-05-31 01:09:22 +0100

    add Task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """hash_password function"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid function"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
