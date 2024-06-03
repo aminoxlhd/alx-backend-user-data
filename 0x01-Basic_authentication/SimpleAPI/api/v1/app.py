d5cca922abb39744f03a32bfef703baff22fed81
aminoxlhd <aminoxlhd@gmail.com>
2024-06-03 18:06:50 +0100

    pycodestyle

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
