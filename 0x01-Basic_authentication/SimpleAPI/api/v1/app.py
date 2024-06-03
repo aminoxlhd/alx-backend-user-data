5f753ed5db25e3463ae84c656c81d6882a74b547
aminoxlhd <aminoxlhd@gmail.com>
2024-06-03 18:06:28 +0100

    add Task 0

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
