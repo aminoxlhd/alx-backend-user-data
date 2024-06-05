2ea54acc207c643d86c1249fa67815535d270f87
aminoxlhd <aminoxlhd@gmail.com>
2024-06-05 01:46:09 +0100

    edit Task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,9 +6,9 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-import os
 
 
 app = Flask(__name__)
@@ -19,15 +19,15 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 @app.before_request
 def before_request_func():
     """before_request_func function"""
-    if auth is None:
+    if Auth is None:
         return
-    if not auth.require_auth(request.path, ['/api/v1/status/',
+    if not Auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
                                             '/api/v1/forbidden/']):
         return
-    if auth.authorization_header(request) is None:
+    if Auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+    if Auth.current_user(request) is None:
         abort(403)
 
 
