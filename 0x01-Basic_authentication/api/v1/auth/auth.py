692ee0f3f38f3763cd6c0fd31b12840d1a3063a9
aminoxlhd <aminoxlhd@gmail.com>
2024-06-05 01:01:21 +0100

    add Task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""manage the API authentication"""
+from flask import request
+from typing import List, typeVa
+
+
+class Auth():
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth function"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header function"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user function"""
+        return None
