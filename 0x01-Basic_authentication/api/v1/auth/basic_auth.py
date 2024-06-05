bf6cacc614067038e2a3aca976d89888358d55fa
aminoxlhd <aminoxlhd@gmail.com>
2024-06-05 21:53:32 +0100

    add Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """ Basic auth"""
+import base64
+import binascii
+
 from api.v1.auth.auth import Auth
 
 
@@ -15,3 +18,15 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header function"""
+        b64_auth_header = base64_authorization_header
+        if b64_auth_header and isinstance(b64_auth_header, str):
+            try:
+                encode = b64_auth_header.encode('utf-8')
+                base = base64.b64decode(encode)
+                return base.decode('utf-8')
+            except binascii.Error:
+                return None
