d60b251a20332a9daa4631964fa790332e277d20
aminoxlhd <aminoxlhd@gmail.com>
2024-06-07 02:28:02 +0100

    edit task 2/3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 """session auth"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
     """SessionAuth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create_session function"""
+        if not user_id or type(user_id) != str:
+            return
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[user_id] = session_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user_id_for_session_id function"""
+        if not session_id or type(session_id) != str:
+            return
+        return SessionAuth.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user function"""
+        if request:
+            session_cookie = self.session_cookie(request)
+            if session_cookie:
+                user_id = self.user_id_for_session_id(session_cookie)
+                return User.get(user_id)
