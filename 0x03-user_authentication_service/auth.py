57337b65d39d3925fc94009e66d5eaa0f1bc3e0a
aminoxlhd <aminoxlhd@gmail.com>
2024-06-13 22:32:28 +0100

    add task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+auth
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """_hash_password function"""
+    hashed_bytes = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed_bytes
