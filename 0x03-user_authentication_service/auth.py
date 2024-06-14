53e52d6b31e1059c3e5fc65d1ff9d4b6259709b0
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 19:17:30 +0100

    add task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,11 @@ class Auth:
             return new_session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """get_user_from_session_id function"""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
