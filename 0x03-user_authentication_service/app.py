d201cdae7e5f61ebb2c97dfc7608b178c6617912
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 01:17:51 +0100

    edit task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,5 +26,6 @@ def users() -> Response:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
