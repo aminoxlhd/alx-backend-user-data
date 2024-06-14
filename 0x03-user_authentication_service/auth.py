d9360af9bf8714b2c3291374ceb7181c9a6edab3
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 19:10:56 +0100

    edit task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,7 +38,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(
                 email, hashed_password.decode('utf-8'))
             return new_user
