b3a2c027d31ca7b28c844ef4593d2f6c43801468
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 19:54:23 +0100

    add task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -96,4 +96,4 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(user.id, password=password)
         except NoResultFound:
-            return None
+            raise ValueError()
