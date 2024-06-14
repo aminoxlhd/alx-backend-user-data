88d5ef5013fc6da05346bf85946fc048b9af63df
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 01:16:31 +0100

    add task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -12,6 +14,17 @@ def hello_world():
     """hello_world function"""
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route("/users", methods=["POST"])
+def users() -> Response:
+    """users function"""
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
