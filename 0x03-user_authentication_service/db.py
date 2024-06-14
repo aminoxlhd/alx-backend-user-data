037e9b3c2b02f7875539614a4a84249e166142e7
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 18:58:17 +0100

    edit task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,7 +37,15 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """find_user_by function"""
+        """Find a user by specified attributes.
+
+        Raises:
+            error: NoResultFound: When no results are found.
+            error: InvalidRequestError: When invalid query arguments are passed
+
+        Returns:
+            User: First row found in the `users` table.
+        """
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
