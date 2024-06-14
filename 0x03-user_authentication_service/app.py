7ab2c41a3a9ac935a2f8c924b458674c05c5cd8c
aminoxlhd <aminoxlhd@gmail.com>
2024-06-14 01:21:14 +0100

    edit task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -16,15 +16,18 @@ def hello_world():
 
 
 @app.route("/users", methods=["POST"])
-def users() -> Response:
-    """users function"""
-    email = request.form["email"]
-    password = request.form["password"]
+def register_user():
+    """register_user function"""
     try:
+        email = request.form["email"]
+        password = request.form["password"]
+
         user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+
+        return jsonify({"email": user.email, "message": "user created"}), 200
+
+    except ValueError as err:
+        return jsonify({"message": str(err)}), 400
 
 
 if __name__ == "__main__":
